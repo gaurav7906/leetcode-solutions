23a7ff472f295aa132596a928b0e82877e464d07
gaurav7906 <104683487+gaurav7906@users.noreply.github.com>
2025-03-05 11:57:51 +0530

    Time: 43 ms (42.98%), Space: 100.1 MB (39.58%) - LeetHub

--- 2035-count-sub-islands/2035-count-sub-islands.cpp
+++ 2035-count-sub-islands/2035-count-sub-islands.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     bool dfs(int row, int col, int n, int m, vector<vector<int>>& vis, vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
         vis[row][col] = 1;
-        bool isSubIsland = (grid1[row][col] == 1);
-
+         bool issubiland=true;
+        if(grid1[row][col]==0){
+             issubiland=false;
+        }
         int drow[] = {-1, 0, 1, 0};
         int dcol[] = {0, 1, 0, -1};
 
@@ -11,11 +13,12 @@ public:
             int nrow = row + drow[i];
             int ncol = col + dcol[i];
 
-            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && !vis[nrow][ncol] && grid2[nrow][ncol] == 1) {
-                isSubIsland &= dfs(nrow, ncol, n, m, vis, grid1, grid2);
+            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && !vis[nrow][ncol] && grid2[nrow][ncol] == 1 ) {
+                if(!dfs(nrow, ncol, n, m, vis, grid1, grid2))
+                issubiland=false;
             }
         }
-        return isSubIsland;
+        return issubiland ;
     }
 
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
